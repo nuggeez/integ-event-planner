3abf78855498411cd2db4a924def14eff99b0131
dlord213 <lollingworld41@gmail.com>
2025-04-26 01:13:26 +0800

    refactors

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -7,11 +7,11 @@ export default function Sidebar(props: { currentPage?: any }) {
   const { currentPage } = props;
 
   return (
-    <aside className="sticky h-screen top-0 flex basis-[15%] flex-col bg-[#242424] px-4">
+    <aside className="sticky top-0 flex h-screen basis-[15%] flex-col bg-[#242424] px-4">
       <h1 className="my-12 text-center text-4xl text-white italic">FROLink</h1>
       <div className="px-2">
         <NavLink
-          to="/"
+          to="/admin"
           className="flex w-full flex-1 cursor-pointer flex-row items-center gap-4 rounded-3xl p-4 hover:bg-white/25"
         >
           <Home size={24} className="text-white" />
@@ -23,41 +23,32 @@ export default function Sidebar(props: { currentPage?: any }) {
             <p className="text-white">All events</p>
           </summary>
           <div className="mt-4 mb-4 ml-8 flex flex-col items-start justify-start gap-6 rounded-3xl bg-white/20 px-8 py-4">
-            <NavLink to="/published-events" className="text-white">
+            <NavLink to="published-events" className="text-white">
               Published events
             </NavLink>
-            <NavLink to="/draft-events" className="text-white">
+            <NavLink to="draft-events" className="text-white">
               Draft events
             </NavLink>
           </div>
         </details>
         <NavLink
-          to="/manage-users"
+          to="manage-users"
           className="flex w-full flex-1 cursor-pointer flex-row items-center gap-4 rounded-3xl p-4 hover:bg-white/25"
         >
           <Users size={24} className="text-white" />
           <p className="text-white">Manage users</p>
         </NavLink>
-        <details className="group">
-          <summary className="flex w-full cursor-pointer list-none flex-row items-center gap-4 rounded-3xl p-4 hover:bg-white/25">
-            <Settings size={24} className="text-white" />
-            <p className="text-white">Settings</p>
-          </summary>
-          <div className="mt-4 ml-8 flex flex-col items-start justify-start gap-6 rounded-3xl bg-white/20 px-8 py-4">
-            <button className="text-white">Published events</button>
-            <button className="text-white">Draft events</button>
-          </div>
-        </details>
+
         <details className="group">
           <summary className="flex w-full cursor-pointer list-none flex-row items-center gap-4 rounded-3xl p-4 hover:bg-white/25">
             <PieChart size={24} className="text-white" />
             <p className="text-white">Analytics</p>
           </summary>
           <div className="mt-4 ml-8 flex flex-col items-start justify-start gap-6 rounded-3xl bg-white/20 px-8 py-4">
-            <NavLink to="/performance" className="text-white">
+            <NavLink to="performance" className="text-white">
               Performance
             </NavLink>
-            <NavLink to="/users-activity" className="text-white">
+            <NavLink to="users-activity" className="text-white">
               User Activity
             </NavLink>
             <button className="text-white">Export Reports</button>
